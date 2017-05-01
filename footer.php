be6c1ac7f142fcce40ff8e8d4b32d6aea4daa482
Unknown <kevnl_95@hotmail.com>
2017-05-01 15:16:47 +0200

    vikingpmc page

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!--FOOTER-->
+<footer class="footer">
+<div class="container">
+        <div class="col-md-12 center">
+          <a target="_blank" href="http://paypal.me/VIKINGWARDADDY"><i class="fa fa-cc-paypal fa-5x" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://www.twitch.tv/vknpmc"><i class="fa fa-twitch fa-5x" aria-hidden="true"></i></a>
+          <a target="_blank" href="https://twitter.com/VKNPMC"><i class="fa fa-twitter fa-5x" aria-hidden="true"></i></a>
+        </div>
+</div>
+    <a href="#" class="back-to-top"><i class="fa fa-angle-double-up fa-4x" style="color:black" aria-hidden="true"></i></a>
+
+</footer>
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://use.fontawesome.com/d74b8e18b3.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scrollUp.js"></script>
+<script src="js/sidenav.js"></script>
+<script src="js/event.js"></script>
+<script src="js/tooltip.js"></script>
+</body>
+
+</html>
---
+++ js/sidenav.js
@@ -0,0 +1,33 @@
+
+function openNavOperation() {
+    document.getElementById("operationsNav").style.width = "100%";
+}
+
+function closeNavOperation() {
+    document.getElementById("operationsNav").style.width = "0";
+}
+
+function openNavCommunication() {
+    document.getElementById("communicationsNav").style.width = "100%";
+}
+
+function closeNavCommunication() {
+    document.getElementById("communicationsNav").style.width = "0";
+}
+
+
+function openNavContact() {
+    document.getElementById("contactUsNav").style.width = "100%";
+}
+
+function closeNavContact() {
+    document.getElementById("contactUsNav").style.width = "0";
+}
+
+function openNavTournament() {
+    document.getElementById("tournamentNav").style.width = "100%";
+}
+
+function closeNavTournament() {
+    document.getElementById("tournamentNav").style.width = "0";
+}
